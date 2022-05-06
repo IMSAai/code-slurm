f76081bb78d8701d1abc2d735473fd3ab5f188e6
Dev Singh <dev@devksingh.com>
2022-05-06 12:24:06 -0500

    fix compat with new ingress

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name = "code-slurm",
-    version = "1.0.0",
+    version = "2.0.0",
     author = "EpochML",
     author_email = "webmaster@epochml.org",
     maintainer = "EpochML",
